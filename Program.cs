59f3836f3a6b31cb0ae87c60a9041dcc43241e08
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-03-01 18:08:09 -0500

    Rtn JSON files

--- Program.cs
+++ Program.cs
@@ -9,29 +9,30 @@ namespace files_module
         static void Main(string[] args)
         {
             var currentDirectory = Directory.GetCurrentDirectory();
-           
             var storesDirectory = Path.Combine(currentDirectory, "stores");
 
-            var files = FindFiles(storesDirectory);
+            var salesFiles = FindFiles(storesDirectory);
 
-            foreach (var file in files)
+            foreach (var file in salesFiles)
             {
                 Console.WriteLine(file);
             }
         }
+
         static IEnumerable<string> FindFiles(string folderName)
         {
             List<string> salesFiles = new List<string>();
-    
+
             var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
-    
+
             foreach (var file in foundFiles)
             {
-                if (file.EndsWith("sales.json"))
-                {
+                var extension = Path.GetExtension(file);
+
+                if (extension == ".json")
                     salesFiles.Add(file);
-                }
             }
+
             return salesFiles;
         }
     }
